e1deeff2a50099be0aed4fd11991bc786835cd9c
Bahriye İşgör <122686947+Bhryee@users.noreply.github.com>
2025-02-22 22:37:09 +0300

    Add KDV_Calculator

---
+++ KDV_Calculator/kdv_calculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class kdv_calculator {
+    public static void main(String[] args) {
+        double tutar, kdvTutar, kdvOran = 0.18, kdvliTutar ;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret tutarını giriniz: ");
+        tutar = input.nextDouble();
+
+        kdvTutar = tutar + kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        if(tutar < 1000){
+            kdvOran = 0.18;
+
+        }else{
+            kdvOran = 0.08;
+        }
+
+        System.out.println("KDV'siz tutar: " + tutar);
+        System.out.println("KDV oranı: " + kdvOran);
+        System.out.println("KDV tutarı: " + kdvTutar);
+        System.out.println("KDV'li tutar: " + kdvliTutar);
+    }
+}
